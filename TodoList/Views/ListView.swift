11903accc451262c9d4a8b20b59161b689d513b0
Jon Doe <filbrodaga@gmail.com>
2025-05-19 12:54:23 +0300

    Some

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Text("Hi")
+        }
+        .navigationTitle("Todo List 📝")
     }
 }
 
 #Preview {
-    ListView()
+    NavigationView{
+        ListView()
+    }
 }
