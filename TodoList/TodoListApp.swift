4a91571dceda1fe84ddeb531429b1a823a6aba6d
Jon Doe <filbrodaga@gmail.com>
2025-05-19 09:49:11 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by FIL on 19.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
