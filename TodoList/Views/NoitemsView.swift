4a1504500b7b330bf4b2d01ac98579aeb8ca594d
Jon Doe <filbrodaga@gmail.com>
2025-05-20 11:47:00 +0300

    Some #5

---
+++ TodoList/Views/NoitemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoitemsView.swift
+//  TodoList
+//
+//  Created by FIL on 20.05.2025.
+//
+
+import SwiftUI
+
+struct NoitemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoitemsView()
+}
